eee0efc6945b8a0cb6c8b51656a8104bd85d278f
JanisMuiznieks <janis.muiznieks8@gmail.com>
2021-02-20 12:43:07 +0200

    Login app

---
+++ LoginApp/Extension/Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Extension.swift
+//  LoginApp
+//
+//  Created by janis.muiznieks on 19/02/2021.
+//
+
+import UIKit
+
+extension UIViewController{
+    func warningPopUp(withTitle title: String?, withMessage message: String?){
+        DispatchQueue.main.async {
+            let popUp = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            popUp.addAction(okButton)
+            self.present(popUp, animated: true)
+            
+        }
+    }
+}
